2cfe52f0e0928b18505c8d4a1094d01af3c7422c
codewithsw0rd <thugkaustuv@gmail.com>
2026-03-24 11:03:30 +0545

    moved profile to abstract

--- accounts/models.py
+++ accounts/models.py
@@ -64,6 +64,9 @@ class Profile(BaseModel):
     
     address = models.TextField(null=True, blank=True)
     
+    class Meta:
+        abstract = True
+    
 class StudentProfile(Profile):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='studentprofile')
     roll_number = models.CharField(max_length=20, unique=True)
