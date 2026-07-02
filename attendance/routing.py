33819a9b582bc9207ec4bd25da0b595d614640c6
Kranti Kumar Shah <Krantikumar803@gmail.com>
2026-07-02 16:53:18 +0545

    some new big chnages

--- attendance/routing.py
+++ attendance/routing.py
@@ -6,9 +6,17 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
+    # Teacher attendance streaming
     re_path(
         r'ws/attendance/stream/(?P<session_id>[\w-]+)/$',
         consumers.AttendanceStreamConsumer.as_asgi(),
         name='attendance_stream'
+    ),
+    
+    # Student real-time notifications
+    re_path(
+        r'ws/notifications/$',
+        consumers.StudentNotificationConsumer.as_asgi(),
+        name='student_notifications'
     ),
 ]
